c7ff6afbf7ed897f7ab3e04d2dafcf2cfebf6077
mroscardd <mroscardd@gmail.com>
2026-04-10 22:14:47 +0200

    protected views

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('restaurant/', include('restaurant.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
+    
 
 ]
--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .import views
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'tables', views.BookingView)
@@ -10,4 +11,5 @@ urlpatterns= [
     path('menu/', views.MenuItemsView.as_view(), name="menu"),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view(), name="menu_item"),
     path('booking/', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token)
 ]
